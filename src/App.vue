cb784aa591bc660832a7ff199aaba97cbfbc601c
aaronLejeune <aaron@orangeorca.be>
2021-08-17 22:22:49 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 
 <template>
   <div class="min-h-screen bg-gray-50">
+    <header class="relative bg-white">
+        <p class="bg-indigo-600 h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8">
+          Funfoo Development Env.
+        </p>
+    </header>
     <router-view></router-view>
   </div>
 </template>
--- src/helpers/router.js
+++ src/helpers/router.js
@@ -4,6 +4,13 @@ import Login from "@/pages/Login.vue";
 import Register from "@/pages/Register.vue";
 import Order from "@/pages/Order.vue";
 
+//steps
+import Step1 from '@/pages/flow/1_NumberKids.vue'
+import Step2 from '@/pages/flow/2_Delivery.vue'
+import Step3 from '@/pages/flow/3_Payment.vue'
+import Step4 from '@/pages/flow/4_Confirmation.vue'
+import Step5 from '@/pages/flow/5_Thanks.vue'
+
 const routes = [
   {
     path: "/",
@@ -25,6 +32,28 @@ const routes = [
     path: "/order",
     name: "Order",
     component: Order,
+    children: [
+      {
+        path: '/step-1',
+        component: Step1
+      },
+      {
+        path: '/step-2',
+        component: Step2
+      },
+      {
+        path: '/step-3',
+        component: Step3
+      },
+      {
+        path: '/step-4',
+        component: Step4
+      },
+      {
+        path: '/step-5',
+        component: Step5
+      }
+    ]
   },
 ];
 
