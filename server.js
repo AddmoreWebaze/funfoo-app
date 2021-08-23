7edf79a56c5972b2d4c88bd91d72c53b8bf38cb5
aaronLejeune <aaron@orangeorca.be>
2021-08-23 15:10:28 +0200

    prepare for heroku

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 require('dotenv').config()
 
 const express = require('express')
+const serveStatic = require('serve-static')
 const path = require('path')
-const ejs = require('ejs')
+const port = Number(process.env.VUE_APP_PORT) || 8080
 
 const app = express()
 
-app.engine('html', ejs.renderFile)
-app.set('view engine', 'html')
-app.set('views', path.join(__dirname, '/dist'))
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 app.use(express.static(path.join(__dirname, '/dist')))
 
+/*
 app.get('/', (req, res) => {
   res.render('index');
-})
+})*/
 
-app.listen(Number(process.env.VUE_APP_PORT), () => {
-  console.log(`Running on port: ${process.env.VUE_APP_PORT}`)
+
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`)
 })
