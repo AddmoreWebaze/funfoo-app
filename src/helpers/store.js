1e9d0bec1b75930c8140be2f71d1fe2a57afe5ac
aaronLejeune <aaron@orangeorca.be>
2021-08-30 18:02:43 +0200

    major changes

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -2,23 +2,12 @@ import { createStore } from 'vuex'
 import axios from 'axios'
 //import router from './router';
 
-const encrypt = (salt, text) => {
-  console.log(salt, text)
-  const textToChars = text => text.split('').map(c => c.charCodeAt(0));
-  const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
-  const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
-  return text.split('').map(textToChars).map(applySaltToChar).map(byteHex).join('');
-};
+import userModule from './modules/user'
+
 
 export const store = createStore({
   state () {
     return {
-
-        //auth 
-        status: '',
-        token: localStorage.getItem('token') || '',
-        user : {},
-
 
         //funfoo selected product
         product: {
@@ -31,6 +20,9 @@ export const store = createStore({
             { id: 1, name: 'Funfoo', routeName: 'home' },
             { id: 2, name: 'Box', routeName: 'getting-started' },
           ],
+
+          pricing:{
+          },
 
           //number of kids
           kidsCount: [
@@ -63,108 +55,77 @@ export const store = createStore({
           deliveryInstruction: '',
           kids: '',
           adults: '',
-        }
-
-        
+        },        
     }
   },
   mutations: {
-    auth_request(state){
-      state.status = 'loading'
-    },
-    auth_success(state, token, user){
-      state.status = 'success'
-      state.token = token
-      state.user = user
-    },
-    auth_error(state){
-      state.status = 'error'
-    },
-    logout(state){
-      state.status = ''
-      state.token = ''
+
+    setQty(state, {kids, adults}){
+      state.order.kids = kids
+      state.order.adults = adults
     },
 
+    setDelivery(state, form){
+      state.order.lname = form.lname
+      state.order.street = form.street
+      state.order.hnumber = form.hnumber
+      state.order.city = form.city
+      state.order.zip = form.zip
+      state.order.phone = form.phone
+      state.order.deliveryMoment = form.deliveryMoment
+      state.order.deliveryInstruction = form.deliveryInstruction
+      state.order.firstDelivery = form.firstDelivery
+    }
   },
   actions: {
-    //login action
-    login({commit}, user){
-      return new Promise((resolve, reject) => {
-        commit('auth_request')
-        
-        user.password = encrypt(process.env.VUE_APP_SALT, user.password)
+    
+    //order
+    //cahnge the QTY of your box
+    changeQty({ commit }, form){
+      commit('setQty', form)
+    },
 
-        axios({url: process.env.VUE_APP_API_URL + '/login', data: user, method: 'POST' })
+    //order
+    //complete user profile
+    changeDelivery({ commit }, form){
+      return new Promise((resolve, reject) => {
+        commit('setDelivery', form)
+
+        var completeProfile = this.state.order
+        completeProfile.apikey = localStorage.getItem('token')
+
+        axios({url: process.env.VUE_APP_API_URL + '/user/complete', data: completeProfile, method: 'POST' })
         .then(resp => {
-          console.log(resp)
-          const token = resp.data.apikey
-          const user = resp.data.user
-          localStorage.setItem('token', token)
-          axios.defaults.headers.common['Authorization'] = token
-          commit('auth_success', token, user)
           resolve(resp)
         })
         .catch(err => {
-          commit('auth_error')
-          console.log(err)
-          localStorage.removeItem('token')
           reject(err)
         })
       })
     },
 
-    register({commit}, user){
-      return new Promise((resolve, reject) => {
-        commit('auth_request')
-
-        user.password = encrypt(process.env.VUE_APP_SALT, user.password)
-
-        axios({url: process.env.VUE_APP_API_URL + '/user/new', data: user, method: 'POST' })
-        .then(resp => {
-          const token = resp.data.apikey
-          const user = resp.data.user
-          localStorage.setItem('token', token)
-          axios.defaults.headers.common['Authorization'] = token
-          commit('auth_success', token, user)
-          resolve(resp)
-        })
-        .catch(err => {
-          commit('auth_error', err)
-          localStorage.removeItem('token')
-          reject(err)
-        })
-      })
-    },
-
-    logout({commit}){
-      return new Promise((resolve) => {
-        commit('logout')
-        localStorage.removeItem('token')
-        delete axios.defaults.headers.common['Authorization']
-        resolve()
-      })
-    },
     //depending on the chosen method, 
     //create a new payment route
     createPayment({commit}, { method, IBAN = null }){
       return new Promise((resolve, reject) => {
         commit('auth_request')
-        //var token = localStorage.getItem('token')
-        var token = 'f16e04b3c4435944591f5e2c339a5f25ddf0494876f778c597a4c2525ecae680'
+        var token = localStorage.getItem('token')
 
-        axios({url: process.env.VUE_APP_API_URL + '/mandate/create', data: { 
-          apikey: token,
-          redirectUrl: `${process.env.VUE_APP_BASE_URL}/order/step-4`,
-          method: method,
-          iban: IBAN
+        axios({url: process.env.VUE_APP_API_URL + '/mandate/create', 
+          data: { 
+            apikey: token,
+            redirectUrl: `${process.env.VUE_APP_BASE_URL}/order/step-4`,
+            method: method,
+            iban: IBAN
           }, method: 'POST' })
         .then(resp => {
-          if(IBAN == null && resp.data.valid){
+          if(method == 'directdebit' && resp.data.valid){
+            resolve('this is testing')
+          }else if(resp.data.valid) {
             document.location.href = resp.data.authorization
           }else{
             reject('Api not reached')
           }
-          resolve(resp)
         })
         .catch(err => {
           reject(err)
@@ -173,7 +134,10 @@ export const store = createStore({
     },
   },
   getters: {
-    isLoggedIn: state => !!state.token,
-    authStatus: state => state.status,
+    getOrder: state => state.order
+  },
+
+  modules: {
+    userModule,
   }
 })
